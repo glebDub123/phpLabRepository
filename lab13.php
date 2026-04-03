a865243021115d4ce643c6193c21bac15b45e065
glebDub123 <rozikabdu871.gmail.com>
2026-04-03 14:04:51 +0300

    создание методов получения свойств

--- lab13.php
+++ lab13.php
@@ -1,14 +1,24 @@
 <?php
 
     //Задание 1
-    class Employee{
+    class SvoiEmployee{
         public $name;
         public $age;
         public $salary;
+
+        public function getName(){
+            return $this->name;
+        } 
+        public function getAge(){
+            return $this->age;
+        }
+        public function getSalary(){
+            return $this->salary;
+        }
     }
 
-    $employee1 = new Employee();
-    $employee2 = new Employee();
+    $employee1 = new SvoiEmployee();
+    $employee2 = new SvoiEmployee();
 
     $employee1->name = "Глеб1";
     $employee1->age = 19;
@@ -23,4 +33,10 @@
     echo "Сумма возрастов работников: ".$employee1->age+$employee2->age."</br>";
     echo "Сумма зарплат работников: ".$employee1->salary+$employee2->salary."</br>";
 
+    //Задание 3-5
+
+    echo "Имя - ".$employee1->getName()."</br>";
+    echo "Возраст - ".$employee1->getAge()."</br>";
+    echo "Зарплата - ".$employee1->getSalary()."</br>";
+    
 ?>
