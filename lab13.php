f29393fd1637eb7fa2122fb91339a5bbcabcd51a
glebDub123 <rozikabdu871.gmail.com>
2026-04-03 14:17:17 +0300

    изменение метода getSalary, изменение метода getAge на setAge

--- lab13.php
+++ lab13.php
@@ -3,17 +3,22 @@
     //Задание 1
     class SvoiEmployee{
         public $name;
-        public $age;
+        private $age;
         public $salary;
 
         public function getName(){
             return $this->name;
         } 
-        public function getAge(){
-            return $this->age;
+        public function setAge($newAge){
+            $this->age = $newAge;
         }
-        public function getSalary(){
-            return $this->salary;
+
+        public function getSalary($employees){
+            $sumSalary = 0;
+            foreach ($employees as $employee) {
+                $sumSalary += $employee->salary;
+            }
+            return $sumSalary;
         }
     }
 
@@ -21,22 +26,27 @@
     $employee2 = new SvoiEmployee();
 
     $employee1->name = "Глеб1";
-    $employee1->age = 19;
+    //$employee1->age = 19;
     $employee1->salary = 100;
 
     
     $employee2->name = "Глеб2";
-    $employee2->age = 12;
+    //$employee2->age = 12;
     $employee2->salary = 20000;
 
     //Задание 2
-    echo "Сумма возрастов работников: ".$employee1->age+$employee2->age."</br>";
+    //echo "Сумма возрастов работников: ".$employee1->age+$employee2->age."</br>";
     echo "Сумма зарплат работников: ".$employee1->salary+$employee2->salary."</br>";
 
     //Задание 3-5
 
     echo "Имя - ".$employee1->getName()."</br>";
-    echo "Возраст - ".$employee1->getAge()."</br>";
-    echo "Зарплата - ".$employee1->getSalary()."</br>";
+
+    //Задание 6
+    echo "Зарплата всех работников - ".$employee1->getSalary([$employee1,$employee2])."</br>";
+    
+    //Задание 7
+    $employee1->setAge(55);
+    
     
 ?>
