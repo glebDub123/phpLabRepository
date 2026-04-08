8da79b8a9365c0c7d2eef24522052507735d8350
glebDub123 <rozikabdu871.gmail.com>
2026-04-08 21:42:46 +0300

    создан класс blogpage и карточки с выбором чаем и кофе

--- lab14.php
+++ lab14.php
@@ -1,7 +1,7 @@
 <?php
 class Page {
-    private string $name;
-    private string $template;
+    protected string $name;
+    protected string $template;
 
     public function __construct() {
         $this->name = "page";
@@ -12,4 +12,47 @@ class Page {
         echo $this->template;
     }
 }
+
+class BlogPage extends Page{
+    public function __construct() {
+        $this->name = "blog";
+        $this->template = '
+        <div class="cards-container">
+        <div class="card">
+            <h3>Чай</h3>
+            <p>Успокаивающий напиток</p>
+            <button>Выбрать чай</button>
+        </div>
+        
+        <div class="card">
+            <h3>Кофе</h3>
+            <p>Энергичный напиток</p>
+            <button>Выбрать кофе</button>
+        </div>
+    </div>';
+    }
+}
+
 ?>
+
+<style>
+    .cards-container {
+        display: flex;
+    }
+    .card {
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        padding: 20px;
+        width: 200px;
+        text-align: center;
+    }
+    .card button {
+        background: #4CAF50;
+        color: white;
+        border: none;
+        padding: 8px 16px;
+        border-radius: 5px;
+    }
+</style>
+
+
