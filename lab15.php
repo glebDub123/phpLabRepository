65ac6b3c6182c4c5dee4c3ebd58a76fb24636388
glebDub123 <rozikabdu871.gmail.com>
2026-04-09 20:53:43 +0300

    реализация методов получения площади и метода получения информации о фигуре

--- lab15.php
+++ lab15.php
@@ -1,32 +1,74 @@
 <?php
 abstract class Figure {
-    protected integer $area;
+    protected int $area;
     protected string $color;
-    protected integer $sides_count;
+    protected int $sides_count;
 
     abstract public function infoAbout() : string;
 }
 
 class Rectangle extends Figure implements IArea{
-    private integer $a;
-    private integer $b;
-    protected $sides_count = 4;
+    private int $a;
+    private int $b;
+    protected int $sides_count = 4;
+
+    public function __construct($a, $b) {
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function getArea(){
+        return $this->a * $this->b;
+    }
+
+    public function infoAbout() {
+        return "Это класс прямоугольника. У него {$this->sides_count} стороны.";
+    }
 }
 
 class Triangle extends Figure implements IArea{
-    private integer $a;
-    private integer $b;
-    private integer $c;
-    protected $sides_count = 3;
+    private int $a;
+    private int $b;
+    private int $c;
+    protected int $sides_count = 3;
+
+    public function __construct($a, $b, $c) {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+    }
+
+    public function getArea(){
+        $halfPerimetr = ($this->a+$this->b+$this->c)/2;
+        return sqrt($halfPerimetr*($halfPerimter-$this->a)*($halfPerimter-$this->b)*($halfPerimter-$this->c));
+    }
+
+    public function infoAbout() {
+        return "Это класс треугольника. У него {$this->sides_count} стороны.";
+    }
 }
+
 
 class Square extends Figure implements IArea{
-    private integer $a;
-    protected $sides_count = 4;
+    private int $a;
+    protected int $sides_count = 4;
+
+    public function __construct($a) {
+        $this->a = $a;
+    }
+
+    public function getArea(){
+        return $this->a * $this->a;
+    }
+
+    public function infoAbout() {
+        return "Это класс квадрата. У него {$this->sides_count} стороны.";
+    }
 }
 
+
 interface IArea {
-    public function calculateArea() : integer;
+    public function getArea();
 }
 ?>
 
