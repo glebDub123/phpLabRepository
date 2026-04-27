993e6a77b69a706619d786950e1da85c5ffd2afe
glebDub123 <rozikabdu871.gmail.com>
2026-04-27 21:39:07 +0300

    обработка формы контактов

--- pages/contact.php
+++ pages/contact.php
@@ -1,6 +1,9 @@
-<?php include '../includes/header.php'; ?>
+<?php
+include '../includes/header.php';
+?>
 <main>
     <h1>Контакты</h1>
+
     <form method="post">
         <input type="text" name="name" placeholder="Имя">
         <input type="email" name="email" placeholder="Email">
@@ -8,5 +11,14 @@
         <button type="submit">Отправить</button>
     </form>
 </main>
+
+<?php
+    if (isset($_POST['name']) || isset($_POST['email']) || isset($_POST['message'])) {
+        echo '<p>'.$_POST['name'].'</p>';
+        echo '<p>'.$_POST['email'].'</p>';
+        echo '<p>'.$_POST['message'].'</p>';
+        echo '<p>Сообщение отправлено</p>';
+    }
+?>
 <?php include '../includes/footer.php'; ?>
 
